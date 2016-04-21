98834a2c228f19a7d202c9d347bf03bb95a338ad
Daniela Petrova <daniella_petrova@abv.bg>
2016-04-21 10:23:45 +0300

    fix problem with filtering unique projects on dashboard

--- Issue-Tracking-System/app/dashboard/dashboard-controller.js
+++ Issue-Tracking-System/app/dashboard/dashboard-controller.js
@@ -1,7 +1,6 @@
 angular.module('issueTracker.dashboard', [
     'issueTracker.issues.feed',
     'issueTracker.common.filters'
-    //'angularUtils.directives.dirPagination'
 ])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/dashboard', {
@@ -12,8 +11,7 @@ angular.module('issueTracker.dashboard', [
     .controller('DashboardCtrl', [
         '$scope',
         'issuesFeed',
-        '$route',
-        function ($scope, issuesFeed, $route) {
+        function ($scope, issuesFeed) {
 
             $scope.issuesParams = {
                 'pageSize' : 10,
@@ -23,9 +21,24 @@ angular.module('issueTracker.dashboard', [
             $scope.reloadIssues = function() {
                 issuesFeed.getMyIssues($scope.issuesParams.pageSize, $scope.issuesParams.startPage, 'DueDate desc, IssueKey')
                     .then(function (myIssues) {
-                        //console.log(myIssues.data);
+                        console.log(myIssues.data);
                         $scope.myIssues = myIssues.data;
                         $scope.myIssues.TotalCount = $scope.myIssues.TotalPages * $scope.issuesParams.pageSize; //because by default TotalCount: 0 -> BUG?
+
+                        //filter unique projects
+                        var uniqueProjects = [],
+                            keys = [];
+
+                        angular.forEach(myIssues.data.Issues, function(issue) {
+                            var key = issue.Project.Id;
+                            if(keys.indexOf(key) === -1) {
+                                keys.push(key);
+                                uniqueProjects.push(issue.Project);
+                            }
+                        });
+
+                        $scope.affiliatedProjects = uniqueProjects;
+                        console.log($scope.affiliatedProjects)
                     });
             };
 
